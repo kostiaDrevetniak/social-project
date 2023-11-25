09df401de607dd24a2d811939e9e5623921533e5
Kostia <k512110d@gmail.com>
2023-11-25 03:16:45 +0200

    user service impl

--- src/main/java/com/social/project/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/social/project/demo/service/impl/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.social.project.demo.service.impl;
 import com.social.project.demo.model.User;
 import com.social.project.demo.repository.UserRepository;
 import com.social.project.demo.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +17,32 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
-    public List<User> getAll() {
-        return userRepository.findAll();
+    public User create(User user) {
+        return userRepository.save(user);
     }
 
     @Override
-    public User create(User user) {
+    public User readById(UUID id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("User with id " + id + " not found"));
+    }
+
+    @Override
+    public User update(User user) {
+        if (user == null)
+            throw new IllegalArgumentException("User cannot be 'null'");
+        readById(user.getId());
         return userRepository.save(user);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        User user = readById(id);
+        userRepository.delete(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.findAll();
     }
 }
